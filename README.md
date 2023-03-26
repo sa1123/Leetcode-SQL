f3adb18170efc38498b90a13b621f2e1ad399497
Sahil <51375919+sa1123@users.noreply.github.com>
2023-03-26 18:36:07 +0530

    https://leetcode.com/problems/find-total-time-spent-by-each-employee/submissions/922435688/

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Leetcode-SQL
 
 https://leetcode.com/problems/recyclable-and-low-fat-products/submissions/922421670/
+
+https://leetcode.com/problems/find-total-time-spent-by-each-employee/submissions/922435688/
---
+++ find-total-time-spent-by-each-employee.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/find-total-time-spent-by-each-employee/submissions/922435688/
+
+select event_day as day, emp_id, sum(out_time) - sum(in_time) as total_time
+from Employees
+group by emp_id, event_day
